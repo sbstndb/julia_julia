0384f3c3c3f37267e4e4ba1e0cf00e9d3439069b
sbstndb <sebastiendubois4@gmail.com>
2023-09-30 01:31:47 +0200

    Add colorschemes
    	-	 and better performance

--- simple_mandelbrot.jl
+++ simple_mandelbrot.jl
@@ -2,6 +2,9 @@ using LinearAlgebra
 using Images
 using BenchmarkTools
 
+using ColorSchemes
+using Colors
+
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 1
 
 MAX_ITER = 100
@@ -11,13 +14,11 @@ RE_END = -1
 IM_START = -1
 IM_END = 1
 
-WIDTH = 600
-HEIGHT = 400
 
 
-function mandelbrot(c)
-    z = 0 
-    n = 0 
+function mandelbrot(c::Complex{Float64})
+    z::Complex{Float64} = 0 
+    n::Int64 = 0 
     while (abs(z) <= 2) & (n < MAX_ITER)
         z = z*z + c
         n += 1
@@ -30,7 +31,7 @@ function compute_mandelbrot(WIDTH ,HEIGHT)
     result = zeros(WIDTH, HEIGHT)
     for i in 1:WIDTH
         for j in 1:HEIGHT
-            c = RE_START + (i/WIDTH) * (RE_END - RE_START) + ( IM_START + (j/HEIGHT) * (IM_END - IM_START) )*im
+            c::Complex{Float64} = RE_START + (i/WIDTH) * (RE_END - RE_START) + ( IM_START + (j/HEIGHT) * (IM_END - IM_START) )*im
             m = mandelbrot(c)
             color = 255 - floor(m * 255 / MAX_ITER)
             result[i,j] = color
@@ -59,10 +60,30 @@ y = 800
 #bench = @benchmark threaded_compute_mandelbrot(x, y)
 #display(bench)
 
-x = 25600
-y = 25600
+x = 2560
+y = 2560
+
+#x = 10000
+#y = 10000
+
 println("Compute ...")
 result = threaded_compute_mandelbrot(x, y)
 println("Save ...")
-save("gray.png", colorview(Gray, result/255))
+cv = colorview(Gray, result/255)
+cv_rgb = RGB.(cv)
+save("gray.png", cv)
+save("color.png", cv_rgb)
+
+cs  = get(ColorSchemes.leonardo, result/255)
+save("leonardo.png", cs)
+
+
+cs  = get(ColorSchemes.solar, result/255)
+save("solar.png", cs)
+
+cs  = get(ColorSchemes.speed, result/255)
+save("speed.png", cs)
+
+
+
 
